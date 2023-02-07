24545535ce9ac27307feb2d9d59ab94112cbf181
Goh Guan Lin <goh.guan.lin@hotmail.com>
2023-02-07 23:00:51 +0800

    initial commmit for feedback stuff

---
+++ YouthActionDotNet/Migrations/20230202041841_Feedback.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YouthActionDotNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class Feedback : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Feedback",
+                columns: table => new
+                {
+                    FeedbackId = table.Column<string>(type: "TEXT", nullable: false),
+                    FeedbackType = table.Column<string>(type: "TEXT", nullable: true),
+                    FeedbackMessage = table.Column<string>(type: "TEXT", nullable: true),
+                    FeedbackStatus = table.Column<string>(type: "TEXT", nullable: true),
+                    FeedbackDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ServiceCenterId = table.Column<string>(type: "TEXT", nullable: true),
+                    ServiceCenterName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedback", x => x.FeedbackId);
+                    table.ForeignKey(
+                        name: "FK_Feedback_ServiceCenter_ServiceCenterId",
+                        column: x => x.ServiceCenterId,
+                        principalTable: "ServiceCenter",
+                        principalColumn: "ServiceCenterId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedback_ServiceCenterId",
+                table: "Feedback",
+                column: "ServiceCenterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Feedback");
+        }
+    }
+}
---
+++ YouthActionDotNet/Migrations/20230207144150_Feedback_update.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YouthActionDotNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class Feedbackupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FeedbackType",
+                table: "Feedback",
+                newName: "FeedbackSubject");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FeedbackSubject",
+                table: "Feedback",
+                newName: "FeedbackType");
+        }
+    }
+}
