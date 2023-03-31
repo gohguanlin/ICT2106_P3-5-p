976a87d93c7d7d7a94a3b86a270bf1a0c9497ce5
Goh Guan Lin <goh.guan.lin@hotmail.com>
2023-03-31 15:54:22 +0800

    update somemore shit

--- YouthActionDotNet/Models/ExportDocx.cs
+++ YouthActionDotNet/Models/ExportDocx.cs
@@ -8,11 +8,6 @@ namespace YouthActionDotNet.Models
         private string dataAnalysis;
         private string outputReport;
 
-        public ExportDocx(string compiledProgress, string dataAnalysis)
-        {
-            this.compiledProgress = compiledProgress;
-            this.dataAnalysis = dataAnalysis;
-        }
 
         public void exportReport()
         {
--- YouthActionDotNet/Models/ExportPDF.cs
+++ YouthActionDotNet/Models/ExportPDF.cs
@@ -8,12 +8,6 @@ namespace YouthActionDotNet.Models
         private string dataAnalysis;
         private string outputReport;
 
-        public ExportPDF(string compiledProgress, string dataAnalysis)
-        {
-            this.compiledProgress = compiledProgress;
-            this.dataAnalysis = dataAnalysis;
-        }
-
         public void exportReport()
         {
             // Generate PDF report
